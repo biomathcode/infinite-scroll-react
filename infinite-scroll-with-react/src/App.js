5cdc95bfc788fb1a5a0c271c9a916849baa69ee9
Pratik Sharma <sharma.pratik2016@gmail.com>
2022-02-25 12:34:04 +0530

    added infinite-scroll feature 👍🏻

--- infinite-scroll-with-react/src/App.js
+++ infinite-scroll-with-react/src/App.js
@@ -1,26 +1,58 @@
 import { useState } from 'react';
 import './App.css';
 
+import useInfiniteScroll from 'react-infinite-scroll-hook';
+
+
+
+
 function App() {
   const [data, setData] = useState([]);
 
-  const [page, setPage] = useState(0);
+  const [since, setSince] = useState(0);
+  const [limit, setLimit] = useState(10);
 
   const [loading, setLoading] = useState(false);
 
-  const [limit, setLimit] = useState();
 
+  const fetchmore = async (since) => {
+    setSince(since + limit);
+    const response = await fetch(`https://api.github.com/users?since=${since}&per_page=${limit}`);
+    const json = await response.json();
+    setData((data) => [...data, ...json]);
+  }
 
-
+  const [sentryRef] = useInfiniteScroll({
+    loading, 
+    hasNextPage: true,
+    delayInMs:500,
+    onLoadMore: () => {
+      setLoading(true);
+      fetchmore(since);
+      setLoading(false);
+    }
+  })
 
   return (
     <div className="App">
-      {data && data.map((item) => {
-        return (
-          <div key={item.title}>
-          </div>
-        )
-      })}
+      <main className='main'>
+      {data && data.map((item, index) => {
+          return (
+            <div key={index} className='item'>
+              <p>{item && item.login }</p>
+              <img src={item.avatar_url} width={100} height={100} alt={item.avatar_url} />
+            </div>
+          )
+        })}
+        {
+          !loading && 
+          <div ref={sentryRef}>
+          <h1>Loading...</h1>
+        </div>
+        }
+
+      </main>
+     
      
     </div>
   );
